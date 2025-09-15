ba605038197e906b3c6b8ba20c879df4dd8cbd7f
fabiohurtadoc <fabiohurtadoc@gmail.com>
2025-09-15 11:16:51 -0400

    made all migrate

--- core/core_app/settings.py
+++ core/core_app/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
 
+    "customers"
 
 ]
 
@@ -55,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "allauth.account.middleware.AccountMiddleware", 
     "whitenoise.middleware.WhiteNoiseMiddleware",  # <- importante
     "django.contrib.sessions.middleware.SessionMiddleware",
 
